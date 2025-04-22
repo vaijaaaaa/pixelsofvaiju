5039ecf8dfbb4806ccc7c9c244ee55a584d4015b
vaijaaaaa <vaijuwalker111@gmail.com>
2025-04-22 14:36:53 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,307 @@
+import { CodingProfileCard } from "@/components/coding-profiles";
+import { HackathonCard } from "@/components/hackathon-card";
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { ProjectCard } from "@/components/project-card";
+import { ResumeCard } from "@/components/resume-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { DATA } from "@/data/resume";
+import Link from "next/link";
+import Markdown from "react-markdown";
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="gap-2 flex justify-between">
+            <div className="flex-col flex flex-1 space-y-1.5">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                yOffset={8}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+              />
+              <BlurFadeText
+                className="max-w-[600px] md:text-xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Avatar className="size-28 border">
+                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+      <section id="work">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 5}>
+            <h2 className="text-xl font-bold">Work Experience</h2>
+          </BlurFade>
+          {DATA.work.map((work, id) => (
+            <BlurFade
+              key={work.company}
+              delay={BLUR_FADE_DELAY * 6 + id * 0.05}
+            >
+              <ResumeCard
+                key={work.company}
+                logoUrl={work.logoUrl}
+                altText={work.company}
+                title={work.company}
+                subtitle={work.title}
+                href={work.href}
+                badges={work.badges}
+                period={`${work.start} - ${work.end ?? "Present"}`}
+                description={work.description}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+      <section id="education">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <h2 className="text-xl font-bold">Education</h2>
+          </BlurFade>
+          {DATA.education.map((education, id) => (
+            <BlurFade
+              key={education.school}
+              delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+            >
+              <ResumeCard
+                key={education.school}
+                href={education.href}
+                logoUrl={education.logoUrl}
+                altText={education.school}
+                title={education.school}
+                subtitle={education.degree}
+                period={`${education.start} - ${education.end}`}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+      <section id="skills">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="text-xl font-bold">Skills</h2>
+          </BlurFade>
+          <div className="flex flex-wrap gap-1">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                <Badge key={skill}>{skill}</Badge>
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="projects">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  My Projects
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Check out my latest work
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  I&apos;ve worked on a variety of projects, from simple
+                  websites to complex web applications. Here are a few of my
+                  favorites.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+            {DATA.projects.map((project, id) => (
+              <BlurFade
+                key={project.title}
+                delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+              >
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="hackathons">
+        <div className="space-y-12 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 13}>
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                  Hackathons
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  I like building things
+                </h2>
+                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                During my time at university, I participated in 10+ hackathons. It was inspiring to collaborate with talented minds from across the country, building real-world solutions in just a matter of days. These intense, fast-paced events taught me how to think on my feet, work under pressure, and turn ambitious ideas into working prototypes.
+                </p>
+              </div>
+            </div>
+          </BlurFade>
+          <BlurFade delay={BLUR_FADE_DELAY * 14}>
+            <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
+              {DATA.hackathons.map((project, id) => (
+                <BlurFade
+                  key={project.title + project.dates}
+                  delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+                >
+                  <HackathonCard
+                    title={project.title}
+                    description={project.description}
+                    location={project.location}
+                    dates={project.dates}
+                    image={project.image}
+                    links={project.links}
+                  />
+                </BlurFade>
+              ))}
+            </ul>
+          </BlurFade>
+        </div>
+      </section>
+      <section id="coding">
+  <div className="space-y-12 w-full py-12">
+    <BlurFade delay={BLUR_FADE_DELAY * 13}>
+      <div className="flex flex-col items-center justify-center space-y-4 text-center">
+        <div className="space-y-2">
+          <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+            Coding Profiles
+          </div>
+          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+            I like solving problems
+          </h2>
+          <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            During university, I solved hundreds of problems on platforms like LeetCode, CodeChef, and HackerRank. These challenges sharpened my problem-solving skills, helped me optimize code, and taught me to think quickly under pressure.
+          </p>
+        </div>
+      </div>
+    </BlurFade>
+    <BlurFade delay={BLUR_FADE_DELAY * 14}>
+      <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
+        {/* Static Data for coding profiles */}
+        {[
+          {
+            title: "Leetcode Challenges",
+            description: "Solving algorithmic problems on Leetcode.",
+            dates: "Alogorithm Challenges",
+            location: "Online",
+            image: "https://cdn-1.webcatalog.io/catalog/leetcode/leetcode-icon-filled-256.png?v=1714774949349",
+            links: [
+              {
+                icon: <span className="h-4 w-4">🔗</span>,
+                title: "Profile",
+                href: "https://leetcode.com/u/vaijuwalker111/",
+              },
+            ],
+          },
+          {
+            title: "CodeChef Challenges",
+            description: "Competed in various algorithmic challenges on CodeChef.",
+            dates: "Maths Challenges",
+            location: "Online",
+            image: "https://yt3.googleusercontent.com/hN4RNGp3WbykRUGlxIauQ0Rtl55z6uaz1wjB7mllzWa1hGEvIUoQfPWmCBuAkr75HmiwXXi07a4=s900-c-k-c0x00ffffff-no-rj",
+            links: [
+              {
+                icon: <span className="h-4 w-4">🔗</span>,
+                title: "Profile",
+                href: "https://www.codechef.com/users/vaiju",
+              },
+            ],
+          },
+          {
+            title: "GeekforGeeks Competitions",
+            description: "Solved various problems on HackerRank to improve coding skills.",
+            dates: "Competitive Programming",
+            location: "Online",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLGnMqhnl05Dsn2sn0kQoq3Ch2TP9ebmmJsA&s",
+            links: [
+              {
+                icon: <span className="h-4 w-4">🔗</span>,
+                title: "Profile",
+                href: "https://www.geeksforgeeks.org/user/vaijuwalmb33/",
+              },
+            ],
+          },
+        ].map((profile, id) => (
+          <BlurFade
+            key={profile.title + profile.dates}
+            delay={BLUR_FADE_DELAY * 15 + id * 0.05}
+          >
+            <CodingProfileCard
+              title={profile.title}
+              description={profile.description}
+              location={profile.location}
+              dates={profile.dates}
+              image={profile.image}
+              links={profile.links?.map(link => ({
+                ...link,
+                icon: link.icon || <span className="h-4 w-4">🔗</span>, // Default icon
+              }))}
+            />
+          </BlurFade>
+        ))}
+      </ul>
+    </BlurFade>
+  </div>
+</section>
+
+
+      <section id="contact">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Contact
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Get in Touch
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Want to chat? Just shoot me a dm{" "}
+                <Link
+                  href={DATA.contact.social.X.url}
+                  className="text-blue-500 hover:underline"
+                >
+                  with a direct question on twitter
+                </Link>{" "}
+                and I&apos;ll respond whenever I can.
+              </p>
+            </div>
+          </BlurFade>
+        </div>
+      </section>
+    </main>
+  );
+}
---
+++ src/data/resume.tsx
@@ -0,0 +1,272 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+import { title } from "process";
+
+export const DATA = {
+  name: "Vaiju Patil",
+  initials: "DV",
+  url: "https://dillion.io",
+  location: "Bengaluru, KA",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Software Engineer . I love building things and helping people.",
+  summary:
+    "I break things, learn fast, and get things done. Deep into code, computer science, and anything that pushes the boundaries of what’s possible. My journey’s been shaped by curiosity, history, football, and great books — and I’m still chasing mastery every day.",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Javascript",
+    "TailwindCSS",
+    "Node.js",
+    "Python",
+    "SQL",
+    "MongoDB",
+    "Java",
+    "C",
+    "Git",
+    "Figma",
+    "Appwrite",
+    "Prisma",,
+    "Express.js",
+    "Socket.io",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+  ],
+  contact: {
+    email: "vaijuwalker111@gmail.com",
+    tel: "+91 6361557704",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/vaijaaaaa",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/vaiju-patil-585555310/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/Vaijaaaaaaa",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Google Developer Group",
+      href: "https://atomic.finance",
+      badges: [],
+      location: "Remote",
+      title: "UI/UX Designer",
+      logoUrl: "/gdg.png",
+      start: "May 2024",
+      end: "Till now",
+      description:
+        "Designed and implemented UI/UX systems for Google Developer Group (GDG) events and platforms, focusing on user-centered design principles and accessibility. Created responsive, high-fidelity mockups and design systems using Figma. Collaborated with developers to ensure pixel-perfect implementation across web and mobile. Delivered branding and visual assets that elevated community engagement and event experience.",
+    },
+    {
+      company: "Microsoft Learn Student Ambassador",
+      badges: [],
+      href: "https://shopify.com",
+      location: "Remote",
+      title: "Secretary",
+      logoUrl: "/mlsa.png",
+      start: "January 2024",
+      end: "Till now",
+      description:
+        "Served as the Secretary of the Microsoft Learn Student Ambassadors (MLSA) chapter, managing communication, coordination, and event logistics for the community. Maintained detailed documentation of meetings, streamlined collaboration between core teams, and ensured timely execution of initiatives. Played a key role in planning and organizing workshops, hackathons, and outreach programs to grow community engagement and impact.",
+    },
+  ],
+  education: [
+    {
+      school: "Cambridge Institute of Technology",
+      href: "https://engg.cambridge.edu.in/",
+      degree: "Computer Science Engineering - B.E",
+      location: "Bengaluru, KA",
+      logoUrl: "/cit.png",
+      start: "2022",
+      end: "2026",
+    },
+  ],
+  projects: [
+    {
+      title: "Gossip",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the rise of real-time communication apps, I decided to build Gossip — a full-stack chat platform that enables users to engage in seamless, live conversations. It was a great way to explore sockets, authentication, and scalable messaging systems from the ground up.",
+      technologies: [
+        "React.js",
+        "javascript",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "Socket.io",
+        "TailwindCSS",
+        "DasiyUI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://gossip-613v.onrender.com/login",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type:"source",
+          href: "https://github.com/vaijaaaaa/Gossip-A-Real-Time-Chat-Application",
+          icon: <Icons.github className="size-3" />,
+        }
+      ],
+      image: "/gossipproject.png",
+    },
+    {
+      title: "Google Meet Extension",
+      href: "https://magicui.design",
+      dates: "June 2024 - July 2024",
+      active: true,
+      description:
+        " I built a Chrome extension that lets users selectively mute specific meetings without affecting others. Perfect for students and professionals attending overlapping sessions, this tool gives full audio control across tabs — improving focus and reducing distractions.",
+      technologies: [
+        "JavaScript",
+        "Chrome Extension APIs",
+        "HTML/CSS",
+        "Manifest v3",
+        "Chrome Storage API",
+        "React",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/vaijaaaaa/Google-Meet-Links-Mute-Chrome-Extension",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/astra.png",
+     
+    },
+    {
+      title: "Space",
+      href: "https://llm.report",
+      dates: "Feb 2025 - April 2025",
+      active: true,
+      description:
+        "Built a social media app with React Native: Post updates, connect with friends, and explore real-time feeds — designed for fast, smooth performance across iOS and Android.",
+      technologies: [
+       "React Native",
+        "TypeScript",
+        "Expo",
+      "Redux Toolkit",
+        "Convex",
+        "React Navigation",
+        "REST APIs",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/vaijaaaaa/Space",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/Spaceproject.png",
+    },
+    {
+      title: "Jammify",
+      href: "https://automatic.chat",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "Developed a full-stack music streaming app — Jammify, a Spotify clone with real-time playback, custom playlists, and seamless UI powered by modern web tech.",
+        technologies: [
+          "Next.js",
+          "TypeScript",
+          "TailwindCSS",
+          "Prisma",
+          "PostgreSQL",
+          "Shadcn UI",
+          "Zustand",
+          "Stripe",
+          "NextAuth.js",
+          "UploadThing",
+          "Clerk",
+          "React Hook Form",
+        ], 
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/vaijaaaaa/Jammify",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/jammifylogo.png",
+    },
+  ],
+  hackathons: [
+    {
+      title: "CodeVolt 2025",
+      dates: "March 3rd - 4th, 2025",
+      location: "Chennai, Tamil Nadu",
+      description:
+        "Developed an AI-powered automation tool — AutoClaim AI — that streamlines the insurance claim process using intelligent document analysis and decision-making algorithms.",
+      image:
+        "https://d112y698adiu2z.cloudfront.net/photos/production/challenge_thumbnails/003/269/741/datas/large.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [
+        {
+         title:"Github",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/vaijaaaaa/AutoClaim-AI", 
+        }
+      ],
+    },
+    {
+      title: "Great Bengaluru Hackathon",
+      dates: "March 15th - 16th, 2025",
+      location: "Bengaluru, Karnataka",
+      description:
+        "Developed a mobile application — Ecometer — that helps users track and manage household waste, promoting eco-friendly habits through real-time insights and gamified sustainability challenges.",
+      image:
+        "https://cse.noticebard.com/wp-content/uploads/sites/23/2025/02/IMG_2128-1024x683.jpeg",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [
+        {
+          title: "Github",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/vaijaaaaa/EcoMeter",
+        }
+      ],
+    },
+    {
+      title: "GDSC Hackathon",
+      dates: "June 23rd - 24th, 2024",
+      location: "Bengaluru, Karnataka",
+      description:
+        "Participated in a hackathon where I solved 10 real-world problems using Java, focusing on efficiency, scalability, and clean code practices across domains like healthcare, automation, and data handling..",
+      icon: "public",
+      image:
+        "https://s3-h2s-v2.s3.ap-south-1.amazonaws.com/2024-02-17T17%3A06%3A43.180Z-GDSC%20FEST%E2%80%9924%20%2812%29.png",
+      links: [],
+    },
+  ],
+ 
+  
+} as const;
